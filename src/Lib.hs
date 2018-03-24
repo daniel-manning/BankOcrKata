676603dd4dadba2d76a553f53643b2539570e0b6
daniel-manning <daniel.manning@digital.hmrc.gov.uk>
2018-03-24 02:54:15 +0000

    Complete user story 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,26 @@
 module Lib
-    ( createDigit
+    ( createDigit, readAccount, g
     ) where
 
-createDigit::String -> Int
-createDigit input = 2
+import qualified Data.Map.Strict as Map
+import Data.List.Split
+import Data.Maybe
+import Data.Char
+
+characterMap = Map.fromList [([[0,0,0],[0,0,1],[0,0,1]], 1), ([[0,1,0],[0,1,1],[1,1,0]], 2), ([[0,1,0],[0,1,1],[0,1,1]], 3), ([[0,0,0],[1,1,1],[0,0,1]], 4), ([[0,1,0],[1,1,0],[0,1,1]], 5), ([[0,1,0],[1,1,0],[1,1,1]], 6), ([[0,1,0],[0,0,1],[0,0,1]], 7), ([[0,1,0],[1,1,1],[1,1,1]], 8), ([[0,1,0],[1,1,1],[0,1,1]], 9), ([[0,1,0],[1,0,1],[1,1,1]], 0)]
+
+f c = if(c == '|' || c == '_') then 1 else 0
+
+createDigit::[String] -> Maybe Int
+createDigit s = Map.lookup ((map.map) f s) characterMap
+
+readAccount::[String] -> String
+readAccount s = map intToDigit $ catMaybes $ map createDigit (g chunks)
+  where
+    chunks::[[String]]
+    chunks = map (chunksOf 3) s
+
+g::[[[a]]] -> [[[a]]]
+g [] = []
+g ([x]:[y]:[z]:[]) = [[x,y,z]]
+g (x:y:z:[]) = [head x, head y, head z] : g (tail x : tail y : tail z : [])
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,17 +1,133 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
-module Spec where
-
 import Test.Hspec
+import Test.QuickCheck
 import Lib
 
-one = "   \
-      \  |\
-      \  |"
+one = ["   ",
+       "  |",
+       "  |"]
+two = [" _ ",
+       " _|",
+       "|_ "]
+three = [" _ ",
+         " _|",
+         " _|"]
+four = ["   ",
+        "|_|",
+        "  |"]
+five = [" _ ",
+        "|_ ",
+        " _|"]
+six =  [" _ ",
+        "|_ ",
+        "|_|"]
+seven = [" _ ",
+         "  |",
+         "  |"]
+eight = [" _ ",
+         "|_|",
+         "|_|"]
+nine = [" _ ",
+        "|_|",
+        " _|"]
+zero = [" _ ",
+        "| |",
+        "|_|"]
 
-testCreateDigit::String -> Int -> Spec
-testCreateDigit inputstring value = createDigit inputstring `shouldBe` value
+
 
 main :: IO ()
 main = hspec $ do
-  describe "scoreGame" $ do
-    testCreateDigit one 1
+  describe "digit recognition" $ do
+    it "should return the correct digit - 1" $ do
+      createDigit one `shouldBe` Just 1
+
+    it "should return the correct digit - 2" $ do
+      createDigit two `shouldBe` Just 2
+
+    it "should return the correct digit - 3" $ do
+      createDigit three `shouldBe` Just 3
+
+    it "should return the correct digit - 4" $ do
+      createDigit four `shouldBe` Just 4
+
+    it "should return the correct digit - 5" $ do
+      createDigit five `shouldBe` Just 5
+
+    it "should return the correct digit - 6" $ do
+      createDigit six `shouldBe` Just 6
+
+    it "should return the correct digit - 7" $ do
+      createDigit seven `shouldBe` Just 7
+
+    it "should return the correct digit - 8" $ do
+      createDigit eight `shouldBe` Just 8
+
+    it "should return the correct digit - 9" $ do
+      createDigit nine `shouldBe` Just 9
+
+    it "should return the correct digit - 0" $ do
+      createDigit zero `shouldBe` Just 0
+
+  describe "splitter" $ do
+    it "should split up characters" $ do
+      g  [[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]],[[13,14,15],[16,17,18]]] `shouldBe` [[[1,2,3],[7,8,9],[13,14,15]],[[4,5,6],[10,11,12],[16,17,18]]]
+
+  describe "account recognition" $ do
+    it "should be able to read line" $ do
+     readAccount [" _  _  _  _  _  _  _  _  _ ",
+                  "| || || || || || || || || |",
+                  "|_||_||_||_||_||_||_||_||_|"] `shouldBe` "000000000"
+
+    it "should be able to read line" $ do
+     readAccount  ["                           ",
+                   "  |  |  |  |  |  |  |  |  |",
+                   "  |  |  |  |  |  |  |  |  |"] `shouldBe` "111111111"
+    it "should be able to read line" $ do
+     readAccount  [" _  _  _  _  _  _  _  _  _ ",
+                   " _| _| _| _| _| _| _| _| _|",
+                   "|_ |_ |_ |_ |_ |_ |_ |_ |_ "] `shouldBe` "222222222"
+
+    it "should be able to read line" $ do
+      readAccount  [" _  _  _  _  _  _  _  _  _ ",
+                    " _| _| _| _| _| _| _| _| _|",
+                    " _| _| _| _| _| _| _| _| _|"] `shouldBe` "333333333"
+
+    it "should be able to read line" $ do
+     readAccount  ["                           ",
+                   "|_||_||_||_||_||_||_||_||_|",
+                   "  |  |  |  |  |  |  |  |  |"] `shouldBe` "444444444"
+
+    it "should be able to read line" $ do
+     readAccount  [" _  _  _  _  _  _  _  _  _ ",
+                   "|_ |_ |_ |_ |_ |_ |_ |_ |_ ",
+                   " _| _| _| _| _| _| _| _| _|"] `shouldBe` "555555555"
+
+    it "should be able to read line" $ do
+         readAccount  [" _  _  _  _  _  _  _  _  _ ",
+                       "|_ |_ |_ |_ |_ |_ |_ |_ |_ ",
+                       "|_||_||_||_||_||_||_||_||_|"] `shouldBe` "666666666"
+
+    it "should be able to read line" $ do
+         readAccount  [" _  _  _  _  _  _  _  _  _ ",
+                       "  |  |  |  |  |  |  |  |  |",
+                       "  |  |  |  |  |  |  |  |  |"] `shouldBe` "777777777"
+
+    it "should be able to read line" $ do
+         readAccount  [" _  _  _  _  _  _  _  _  _ ",
+                       "|_||_||_||_||_||_||_||_||_|",
+                       "|_||_||_||_||_||_||_||_||_|"] `shouldBe` "888888888"
+
+    it "should be able to read line" $ do
+         readAccount  [" _  _  _  _  _  _  _  _  _ ",
+                       "|_||_||_||_||_||_||_||_||_|",
+                       " _| _| _| _| _| _| _| _| _|"] `shouldBe` "999999999"
+
+    it "should be able to read line" $ do
+         readAccount  ["    _  _     _  _  _  _  _ ",
+                       "  | _| _||_||_ |_   ||_||_|",
+                       "  ||_  _|  | _||_|  ||_| _|"] `shouldBe` "123456789"
+
+    it "should be able to read line" $ do
+         readAccount  [" _  _  _  _  _  _  _  _    ",
+                       "| || || || || || || ||_   |",
+                       "|_||_||_||_||_||_||_| _|  |"]  `shouldBe` "000000051"
