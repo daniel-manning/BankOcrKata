f3b2783bc6e7c60c659f0bc58aac7f0eadabf21d
daniel-manning <daniel.manning@digital.hmrc.gov.uk>
2018-03-30 19:22:26 +0100

    WIP userstory 4

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,45 +1,79 @@
 module Lib
-    ( createDigit, readAccount, g, checksum, isValid, validateString, describeAccount
+    ( createDigit, readAccount, g, checksum, isValid, validateString, describeAccount, everyCharacterWithHammingDistance, readPossibleDigits, readAndTestAccount
     ) where
 
 import qualified Data.Map.Strict as Map
+import Data.List
 import Data.List.Split
 import Data.Maybe
 import Data.Char
+import Control.Monad
 
 characterMap = Map.fromList [([[0,0,0],[0,0,1],[0,0,1]], 1), ([[0,1,0],[0,1,1],[1,1,0]], 2), ([[0,1,0],[0,1,1],[0,1,1]], 3), ([[0,0,0],[1,1,1],[0,0,1]], 4), ([[0,1,0],[1,1,0],[0,1,1]], 5), ([[0,1,0],[1,1,0],[1,1,1]], 6), ([[0,1,0],[0,0,1],[0,0,1]], 7), ([[0,1,0],[1,1,1],[1,1,1]], 8), ([[0,1,0],[1,1,1],[0,1,1]], 9), ([[0,1,0],[1,0,1],[1,1,1]], 0)]
 
-f c = if(c == '|' || c == '_') then 1 else 0
+f c = if c == '|' || c == '_' then 1 else 0
 
 createDigit::[String] -> Maybe Int
 createDigit s = Map.lookup ((map.map) f s) characterMap
 
 readAccount::[String] -> String
-readAccount s = map (maybe '?' intToDigit) $ map createDigit (g chunks)
+readAccount s = map (maybe '?' intToDigit . createDigit) (g chunks)
   where
     chunks::[[String]]
     chunks = map (chunksOf 3) s
 
 g::[[[a]]] -> [[[a]]]
 g [] = []
-g ([x]:[y]:[z]:[]) = [[x,y,z]]
-g (x:y:z:[]) = [head x, head y, head z] : g (tail x : tail y : tail z : [])
+g [[x],[y],[z]] = [[x,y,z]]
+g [x,y,z] = [head x, head y, head z] : g [tail x, tail y, tail z]
 
 checksum::[Int] -> Int
-checksum = ((flip mod 11) . (foldr (\k l ->  l + fst k * snd k) 0) . (zip [1,2..]) . reverse)
+checksum = (flip mod 11) . (foldr (\k l ->  l + uncurry (*) k) 0) . (zip [1,2..]) . reverse
 
 isValid::[Int] -> Bool
 isValid = (== 0).checksum
 
 validateString::String -> Bool
-validateString = isValid . (map digitToInt)
+validateString s = (notElem '?' s) && isValid (map digitToInt s)
 
 extraDescription::String -> String
 extraDescription s | elem '?' s = " ILL"
-                   | not $ validateString s  =  " ERR"
+                   | not $ validateString s = " ERR"
                    | otherwise = ""
 
 describeAccount::[String] -> String
 describeAccount s = k ++ (extraDescription k)
                       where k = (readAccount s)
 
+everyCharacterWithHammingDistance::Int -> [[Int]] -> [Int]
+everyCharacterWithHammingDistance d character = map snd $ filter ((d ==).fst) $ map (\n -> (hammingDistance (concat character) (concat $ fst n), snd n)) $ Map.toList characterMap
+
+hammingDistance::(Eq a) => [a] -> [a] -> Int
+hammingDistance xs ys = length $ filter id $ zipWith (/=) xs ys
+
+--readPossibleDigits::[String] -> [[Int]]
+readPossibleDigits s = map (\l -> (maybeToList (fst l)) ++ (everyCharacterWithHammingDistance 1 (snd l))) $ map (\k -> (createDigit k, ((map.map) f k)))(g chunks)
+  where
+      chunks = map (chunksOf 3) s
+
+--tryAllPossibilities::[[Int]]
+tryAllPossibilities s l = do l1 <- l !! 0
+                             l2 <- l !! 1
+                             l3 <- l !! 2
+                             l4 <- l !! 3
+                             l5 <- l !! 4
+                             l6 <- l !! 5
+                             l7 <- l !! 6
+                             l8 <- l !! 7
+                             l9 <- l !! 8
+                             let r = [l1,l2,l3,l4,l5,l6,l7,l8,l9]
+                             guard $ isValid(r)
+                             guard $ ((==1) $ length $ filter (=='?') s) || ((<= 1) $ hammingDistance r (map digitToInt s))
+                             return r
+
+readAndTestAccount::[String] -> String
+readAndTestAccount k =  if(validateString(s)) then s else  formatChoices s $ (map.map) intToDigit (tryAllPossibilities s $ readPossibleDigits k)
+   where s = readAccount k
+
+formatChoices::String -> [String] -> String
+formatChoices s xs = if((1==) $ length xs) then head xs else (s ++ " AMB " ++ (show $ sort xs))
--- test/Spec.hs
+++ test/Spec.hs
@@ -38,135 +38,210 @@ zero = [" _ ",
 main :: IO ()
 main = hspec $ do
   describe "digit recognition" $ do
-    it "should return the correct digit - 1" $ do
+    it "should return the correct digit - 1" $
       createDigit one `shouldBe` Just 1
 
-    it "should return the correct digit - 2" $ do
+    it "should return the correct digit - 2" $
       createDigit two `shouldBe` Just 2
 
-    it "should return the correct digit - 3" $ do
+    it "should return the correct digit - 3" $
       createDigit three `shouldBe` Just 3
 
-    it "should return the correct digit - 4" $ do
+    it "should return the correct digit - 4" $
       createDigit four `shouldBe` Just 4
 
-    it "should return the correct digit - 5" $ do
+    it "should return the correct digit - 5" $
       createDigit five `shouldBe` Just 5
 
-    it "should return the correct digit - 6" $ do
+    it "should return the correct digit - 6" $
       createDigit six `shouldBe` Just 6
 
-    it "should return the correct digit - 7" $ do
+    it "should return the correct digit - 7" $
       createDigit seven `shouldBe` Just 7
 
-    it "should return the correct digit - 8" $ do
+    it "should return the correct digit - 8" $
       createDigit eight `shouldBe` Just 8
 
-    it "should return the correct digit - 9" $ do
+    it "should return the correct digit - 9" $
       createDigit nine `shouldBe` Just 9
 
-    it "should return the correct digit - 0" $ do
+    it "should return the correct digit - 0" $
       createDigit zero `shouldBe` Just 0
 
   describe "splitter" $ do
-    it "should split up characters" $ do
+    it "should split up characters" $
       g  [[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]],[[13,14,15],[16,17,18]]] `shouldBe` [[[1,2,3],[7,8,9],[13,14,15]],[[4,5,6],[10,11,12],[16,17,18]]]
 
   describe "account recognition" $ do
-    it "should be able to read line" $ do
+    it "should be able to read line" $
      readAccount [" _  _  _  _  _  _  _  _  _ ",
                   "| || || || || || || || || |",
                   "|_||_||_||_||_||_||_||_||_|"] `shouldBe` "000000000"
 
-    it "should be able to read line" $ do
+    it "should be able to read line" $
      readAccount  ["                           ",
                    "  |  |  |  |  |  |  |  |  |",
                    "  |  |  |  |  |  |  |  |  |"] `shouldBe` "111111111"
-    it "should be able to read line" $ do
+
+    it "should be able to read line" $
      readAccount  [" _  _  _  _  _  _  _  _  _ ",
                    " _| _| _| _| _| _| _| _| _|",
                    "|_ |_ |_ |_ |_ |_ |_ |_ |_ "] `shouldBe` "222222222"
 
-    it "should be able to read line" $ do
+    it "should be able to read line" $
       readAccount  [" _  _  _  _  _  _  _  _  _ ",
                     " _| _| _| _| _| _| _| _| _|",
                     " _| _| _| _| _| _| _| _| _|"] `shouldBe` "333333333"
 
-    it "should be able to read line" $ do
+    it "should be able to read line" $
      readAccount  ["                           ",
                    "|_||_||_||_||_||_||_||_||_|",
                    "  |  |  |  |  |  |  |  |  |"] `shouldBe` "444444444"
 
-    it "should be able to read line" $ do
+    it "should be able to read line" $
      readAccount  [" _  _  _  _  _  _  _  _  _ ",
                    "|_ |_ |_ |_ |_ |_ |_ |_ |_ ",
                    " _| _| _| _| _| _| _| _| _|"] `shouldBe` "555555555"
 
-    it "should be able to read line" $ do
+    it "should be able to read line" $
          readAccount  [" _  _  _  _  _  _  _  _  _ ",
                        "|_ |_ |_ |_ |_ |_ |_ |_ |_ ",
                        "|_||_||_||_||_||_||_||_||_|"] `shouldBe` "666666666"
 
-    it "should be able to read line" $ do
+    it "should be able to read line" $
          readAccount  [" _  _  _  _  _  _  _  _  _ ",
                        "  |  |  |  |  |  |  |  |  |",
                        "  |  |  |  |  |  |  |  |  |"] `shouldBe` "777777777"
 
-    it "should be able to read line" $ do
+    it "should be able to read line" $
          readAccount  [" _  _  _  _  _  _  _  _  _ ",
                        "|_||_||_||_||_||_||_||_||_|",
                        "|_||_||_||_||_||_||_||_||_|"] `shouldBe` "888888888"
 
-    it "should be able to read line" $ do
+    it "should be able to read line" $
          readAccount  [" _  _  _  _  _  _  _  _  _ ",
                        "|_||_||_||_||_||_||_||_||_|",
                        " _| _| _| _| _| _| _| _| _|"] `shouldBe` "999999999"
 
-    it "should be able to read line" $ do
+    it "should be able to read line" $
          readAccount  ["    _  _     _  _  _  _  _ ",
                        "  | _| _||_||_ |_   ||_||_|",
                        "  ||_  _|  | _||_|  ||_| _|"] `shouldBe` "123456789"
 
-    it "should be able to read line" $ do
+    it "should be able to read line" $
          readAccount  [" _  _  _  _  _  _  _  _    ",
                        "| || || || || || || ||_   |",
                        "|_||_||_||_||_||_||_| _|  |"]  `shouldBe` "000000051"
 
-    it "should be able to read line" $ do
+    it "should be able to read line" $
          readAccount  ["    _  _  _  _  _  _     _ ",
                        "|_||_|| || ||_   |  |  | _ ",
                        "  | _||_||_||_|  |  |  | _|"]  `shouldBe` "49006771?"
 
   describe "checksum calculation" $ do
-    it "should calculate the correct checksum" $ do
+    it "should calculate the correct checksum" $
       checksum [3,4,5,8,8,2,8,6,5] `shouldBe` 0
 
-    it "should calculate the correct checksum" $ do
+    it "should calculate the correct checksum" $
       checksum [6,6,4,3,7,1,4,9,5] `shouldBe` 2
 
-    it "should be a valid checksum" $ do
+    it "should be a valid checksum" $
       isValid [3,4,5,8,8,2,8,6,5] `shouldBe` True
 
-    it "should validate account strings" $ do
+    it "should validate account strings" $
       validateString "345882865" `shouldBe` True
 
   describe "account description" $ do
-    it "should handle an ill defined string" $ do
+    it "should handle an ill defined string" $
       describeAccount ["    _  _  _  _  _  _     _ ",
                        "|_||_|| || ||_   |  |  | _ ",
                        "  | _||_||_||_|  |  |  | _|"] `shouldBe` "49006771? ILL"
 
-    it "should handle a valid account" $ do
+    it "should handle a valid account" $
       describeAccount [" _  _  _  _  _  _  _  _    ",
                        "| || || || || || || ||_   |",
                        "|_||_||_||_||_||_||_| _|  |"] `shouldBe` "000000051"
 
-    it "should handle a multiple invalid charaters in account" $ do
+    it "should handle a multiple invalid charaters in account" $
       describeAccount ["    _  _     _  _  _  _  _ ",
                        "  | _| _||_| _ |_   ||_||_|",
                        "  ||_  _|  | _||_|  ||_| _ "] `shouldBe` "1234?678? ILL"
 
-    it "should handle an account failing validation" $ do
+    it "should handle an account failing validation" $
       describeAccount [" _  _     _  _        _  _ ",
                        "|_ |_ |_| _|  |  ||_||_||_ ",
                        "|_||_|  | _|  |  |  | _| _|"] `shouldBe` "664371495 ERR"
+
+  describe "everyCharacterWithHammingDistance" $ do
+    it "should find the appropriate characters" $
+       everyCharacterWithHammingDistance 1 [[0,1,0],
+                                            [1,1,0],
+                                            [0,1,1]]  `shouldBe` [6,9]
+--
+--  describe "readPossibleDigits" $ do
+--    it "should find the appropriate characters" $ do
+--       readPossibleDigits ["    _  _     _  _  _  _  _ ",
+--                           "  | _| _||_| _ |_   ||_||_|",
+--                           "  ||_  _|  | _||_|  ||_| _ "]  `shouldBe` [[1],[2],[3],[4],[3,5],[6],[7],[8],[9]]
+
+  describe "readAndTestAccount" $ do
+    it "should find the appropriate string" $
+       readAndTestAccount ["                           ",
+                           "  |  |  |  |  |  |  |  |  |",
+                           "  |  |  |  |  |  |  |  |  |"]  `shouldBe` "711111111"
+
+    it "should find the appropriate string" $
+       readAndTestAccount [" _  _  _  _  _  _  _  _  _ ",
+                           "  |  |  |  |  |  |  |  |  |",
+                           "  |  |  |  |  |  |  |  |  |"]  `shouldBe` "777777177"
+
+    it "should find the appropriate string" $
+       readAndTestAccount [" _  _  _  _  _  _  _  _  _ ",
+                           " _|| || || || || || || || |",
+                           "|_ |_||_||_||_||_||_||_||_|"]  `shouldBe` "200800000"
+
+    it "should find the appropriate string" $
+       readAndTestAccount [" _  _  _  _  _  _  _  _  _ ",
+                           " _| _| _| _| _| _| _| _| _|",
+                           " _| _| _| _| _| _| _| _| _|"]  `shouldBe` "333393333"
+
+    it "should find the appropriate string" $
+       readAndTestAccount [" _  _  _  _  _  _  _  _  _ ",
+                           "|_||_||_||_||_||_||_||_||_|",
+                           "|_||_||_||_||_||_||_||_||_|"]  `shouldBe` "888888888 AMB [\"888886888\",\"888888880\",\"888888988\"]"
+
+    it "should find the appropriate string" $
+       readAndTestAccount [" _  _  _  _  _  _  _  _  _ ",
+                           "|_ |_ |_ |_ |_ |_ |_ |_ |_ ",
+                           " _| _| _| _| _| _| _| _| _|"]  `shouldBe` "555555555 AMB [\"555655555\",\"559555555\"]"
+
+    it "should find the appropriate string" $
+       readAndTestAccount [" _  _  _  _  _  _  _  _  _ ",
+                           "|_ |_ |_ |_ |_ |_ |_ |_ |_ ",
+                           "|_||_||_||_||_||_||_||_||_|"]  `shouldBe` "666666666 AMB [\"666566666\",\"686666666\"]"
+
+    it "should find the appropriate string" $
+       readAndTestAccount [" _  _  _  _  _  _  _  _  _ ",
+                           "|_||_||_||_||_||_||_||_||_|",
+                           " _| _| _| _| _| _| _| _| _|"]  `shouldBe` "999999999 AMB [\"899999999\",\"993999999\",\"999959999\"]"
+
+
+    it "should find the appropriate string" $
+       readAndTestAccount ["    _  _  _  _  _  _     _ ",
+                           "|_||_|| || ||_   |  |  ||_ ",
+                           "  | _||_||_||_|  |  |  | _|"]  `shouldBe` "490067715 AMB [\"490067115\",\"490067719\",\"490867715\"]"
+
+    it "should find the appropriate string" $
+           readAndTestAccount ["    _  _     _  _  _  _  _ ",
+                           " _| _| _||_||_ |_   ||_||_|",
+                           "  ||_  _|  | _||_|  ||_| _|"]  `shouldBe` "123456789"
+
+    it "should find the appropriate string" $
+       readAndTestAccount [" _     _  _  _  _  _  _    ",
+                           "| || || || || || || ||_   |",
+                           "|_||_||_||_||_||_||_| _|  |"]  `shouldBe` "000000051"
+
+    it "should find the appropriate string" $
+       readAndTestAccount ["    _  _  _  _  _  _     _ ",
+                           "|_||_|| ||_||_   |  |  | _ ",
+                           "  | _||_||_||_|  |  |  | _|"]  `shouldBe` "490867715"
